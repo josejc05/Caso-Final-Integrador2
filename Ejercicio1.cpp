737aeb1a3e9cae390f39f9f45f7129d6b8a3f54a
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-28 12:20:10 +0100

    commit 4

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -27,107 +27,23 @@ public:
 };
 
 std::string Variant::to_string() const {
-    switch (type) {
-        case Symbol:
-        case Number:
-        case Cadena:
-            return val;
-        case List: {
-            std::string result = "(";
-            for (const auto& item : list) {
-                result += item.to_string() + " ";
-            }
-            if (!list.empty()) {
-                result.pop_back(); // Remove the trailing space
-            }
-            result += ")";
-            return result;
-        }
-        case Proc:
-            return "<Procedure>";
-        case Lambda:
-            return "<Lambda>";
-        default:
-            return "<Unknown>";
-    }
+    // Implementación de to_string
 }
 
 std::string Variant::to_json_string() const {
-    switch (type) {
-        case Symbol:
-        case Number:
-        case Cadena:
-            return "\"" + val + "\"";
-        case List: {
-            std::string result = "[";
-            for (const auto& item : list) {
-                result += item.to_json_string() + ",";
-            }
-            if (!list.empty()) {
-                result.pop_back(); // Remove the trailing comma
-            }
-            result += "]";
-            return result;
-        }
-        case Proc:
-            return "\"<Procedure>\"";
-        case Lambda:
-            return "\"<Lambda>\"";
-        default:
-            return "\"<Unknown>\"";
-    }
+    // Implementación de to_json_string
 }
 
 Variant Variant::from_json_string(const std::string& sjson) {
-    // Implementación básica, asume que el JSON es una lista
-    std::vector<Variant> result;
-    std::string current;
-    bool in_string = false;
-
-    for (char c : sjson) {
-        if (c == '"') {
-            in_string = !in_string;
-        } else if (c == ',' && !in_string) {
-            result.push_back(Variant(Cadena, current));
-            current.clear();
-        } else if (c != '[' && c != ']' && c != ' ' && c != '\n' && c != '\t' && c != '\r') {
-            current += c;
-        }
-    }
-
-    if (!current.empty()) {
-        result.push_back(Variant(Cadena, current));
-    }
-
-    return Variant(List, result);
+    // Implementación de from_json_string
 }
 
 Variant Variant::parse_json(const std::map<std::string, Variant>& json_map) {
-    // Implementación básica, asume que el JSON es un mapa
-    return Variant(List, json_map.at("value").list);
+    // Implementación de parse_json
 }
 
 int main() {
-    // Ejemplo de uso
-    Variant symbol(Symbol, "x");
-    Variant number(Number, "42");
-    Variant str(Cadena, "Hello, World!");
-    Variant procedure([](const std::vector<Variant>& args) -> Variant { return Variant(Number, "123"); });
-    Variant list(List, {symbol, number, str, procedure});
-
-    std::cout << "Symbol: " << symbol.to_string() << std::endl;
-    std::cout << "Number: " << number.to_string() << std::endl;
-    std::cout << "String: " << str.to_string() << std::endl;
-    std::cout << "Procedure: " << procedure.to_string() << std::endl;
-    std::cout << "List: " << list.to_string() << std::endl;
-
-    // Conversión a cadena JSON
-    std::cout << "JSON String: " << list.to_json_string() << std::endl;
-
-    // Conversión desde cadena JSON
-    std::string json_input = "[\"x\", \"42\", \"Hello, World!\", \"<Procedure>\"]";
-    Variant parsed_variant = Variant::from_json_string(json_input);
-    std::cout << "Parsed Variant: " << parsed_variant.to_string() << std::endl;
-
+    // Código principal
+    // ...
     return 0;
 }
